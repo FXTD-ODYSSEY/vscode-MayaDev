39a4936e4fab5aaa7339ced01cfc3f14c9eef8cd
FXTD-Odyssey <820472580@qq.com>
2019-09-10 20:59:38 +0800

    更新Build

--- out/extension.js
+++ out/extension.js
@@ -12,6 +12,7 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const vscode = __importStar(require("vscode"));
 const cmds_data = __importStar(require("./data/cmds.json"));
+const OpenMaya_1_data = __importStar(require("./data/OpenMaya1.0.json"));
 class TimeUtils {
     static getTime() {
         return new Date()
@@ -55,57 +56,68 @@ exports.Logger = Logger;
 function activate(context) {
     let outputPanel = vscode.window.createOutputChannel('Maya Dev');
     Logger.registerOutputPanel(outputPanel);
-    let pythonExt = vscode.extensions.getExtension('ms-python.python');
-    let api = pythonExt.exports;
-    Logger.info(`pythonExt: ${Object.keys(pythonExt)}`);
-    Logger.info(`pythonExt.id: ${pythonExt.id}`);
-    Logger.info(`pythonExt.extensionPath: ${pythonExt.extensionPath}`);
-    Logger.info(`api: ${Object.keys(api)}`);
-    api.ready.then(values => {
-        Logger.info(`=====================================`);
-        Logger.info(`==========extension ready============`);
-        Logger.info(`=====================================`);
-        Logger.info(`values : ${typeof values}`);
-        Logger.info(`values : ${Object.keys(values)}`);
-        Logger.info(`values.length : ${Object.keys(values).length}`);
-        for (const key in values) {
-            Logger.info(`key : ${key}`);
-            Logger.info(`typeof : ${typeof key}`);
-            Logger.info(`hasOwnProperty : ${values.hasOwnProperty(key)}`);
-        }
-        Logger.info(`=====================================`);
-        Logger.info(`=====================================`);
-        Logger.info(`=====================================`);
-        let pythonConfig = vscode.workspace.getConfiguration("python");
-        let pythonPath = pythonConfig.get("pythonPath");
-        Logger.info(`pythonConfig : ${pythonConfig}`);
-        Logger.info(`pythonConfig keys : ${Object.keys(pythonConfig)}`);
-        for (const key in pythonConfig) {
-            Logger.info(`key : ${key}`);
-            Logger.info(`typeof : ${typeof key}`);
-            Logger.info(`hasOwnProperty : ${values.hasOwnProperty(key)}`);
-        }
-        Logger.info(`pythonPath : ${pythonPath}`);
-    });
-    let mel_completions = [];
-    let cmds_completions = [];
+    // NOTE Python Extension API 测试
+    // let pythonExt = vscode.extensions.getExtension('ms-python.python');
+    // let api = pythonExt.exports;
+    // Logger.info(`pythonExt: ${Object.keys(pythonExt)}`);
+    // Logger.info(`pythonExt.id: ${pythonExt.id}`);
+    // Logger.info(`pythonExt.extensionPath: ${pythonExt.extensionPath}`);
+    // Logger.info(`api: ${Object.keys(api)}`);
+    // api.ready.then(values => {
+    // 	Logger.info(`=====================================`);
+    // 	Logger.info(`==========extension ready============`);
+    // 	Logger.info(`=====================================`);
+    // 	Logger.info(`values : ${typeof values}`);
+    // 	Logger.info(`values : ${Object.keys(values)}`); 
+    // 	Logger.info(`values.length : ${Object.keys(values).length}`); 
+    // 	for (const key in values) {
+    // 		Logger.info(`key : ${key}`); 
+    // 		Logger.info(`typeof : ${typeof key}`); 
+    // 		Logger.info(`hasOwnProperty : ${values.hasOwnProperty(key)}`); 
+    // 	}
+    // 	Logger.info(`=====================================`); 
+    // 	Logger.info(`=====================================`); 
+    // 	Logger.info(`=====================================`); 
+    // 	let pythonConfig = vscode.workspace.getConfiguration("python")
+    // 	let pythonPath = pythonConfig.get("pythonPath")
+    // 	Logger.info(`pythonConfig : ${pythonConfig}`); 
+    // 	Logger.info(`pythonConfig keys : ${Object.keys(pythonConfig)}`); 
+    // 	for (const key in pythonConfig) {
+    // 		Logger.info(`key : ${key}`);
+    // 		Logger.info(`typeof : ${typeof key}`);
+    // 		Logger.info(`hasOwnProperty : ${values.hasOwnProperty(key)}`);
+    // 	}
+    // 	Logger.info(`pythonPath : ${pythonPath}`); 
+    // })
     // NOTE 初始化插件的时候 
     // // NOTE 获取 MEL 数据存放到数组当中
-    // mel_data['completions'].forEach(this_item => {
+    // let mel_completions: Array<vscode.CompletionItem> = [];
+    // mel_data.forEach(this_item => {
     // 	let item = new vscode.CompletionItem(this_item['trigger'], vscode.CompletionItemKind.Function);
     // 	item.detail = this_item['trigger'];
     // 	item.documentation = this_item['comment'];
     // 	mel_completions.push(item);
     // });
     // NOTE 获取 cmds 数据存放到数组当中
-    for (let command in cmds_data['completions']) {
+    let cmds_completions = [];
+    for (let command in cmds_data) {
         let item = new vscode.CompletionItem(command, vscode.CompletionItemKind.Function);
         item.detail = command;
-        // doc = cmds['completions'][command]['instruction'];
-        const instruction = new vscode.MarkdownString(cmds_data['completions'][command]['instruction']);
+        // doc = cmds[command]['instruction'];
+        const instruction = new vscode.MarkdownString(cmds_data[command]['instruction']);
         instruction.isTrusted = true;
         item.documentation = instruction;
         cmds_completions.push(item);
+    }
+    let OpenMaya_1_completions = {};
+    for (let Module in OpenMaya_1_data) {
+        let Class_completions = [];
+        for (let Class in OpenMaya_1_data[Module]) {
+            let item = new vscode.CompletionItem(Class, vscode.CompletionItemKind.Class);
+            item.detail = OpenMaya_1_data[Module][Class]["instruction"];
+            Class_completions.push(item);
+        }
+        OpenMaya_1_completions[Module] = Class_completions;
     }
     function getImportName(documentText, pacakge) {
         let cmds = pacakge;
@@ -125,18 +137,41 @@ function activate(context) {
         }
         return cmds;
     }
-    const cmds_func_compeletion = vscode.languages.registerCompletionItemProvider('python', {
+    // NOTE cmds 函数名快速显示
+    const func_compeletion = vscode.languages.registerCompletionItemProvider('python', {
         provideCompletionItems(document, position) {
-            let cmds = getImportName(document.getText(), "cmds");
-            // NOTE `document.lineAt(position).text`  获取当前光标所在行的文本
+            // NOTE `document.lineAt(position).text`  获取当前光标所在行的文本 并且切掉光标之后的内容
             let linePrefix = document.lineAt(position).text.substr(0, position.character);
-            if (!linePrefix.endsWith(cmds + '.'))
-                return undefined;
-            return [...cmds_completions];
+            // ! -------------------------------
+            // !           cmds 提示
+            // ! -------------------------------
+            let cmds = getImportName(document.getText(), "cmds");
+            if (linePrefix.endsWith(cmds + '.'))
+                return [...cmds_completions];
+            // ! -------------------------------
+            // !        OpenMaya 1.0 提示
+            // ! -------------------------------
+            for (let Module in OpenMaya_1_data) {
+                let OpenMaya = getImportName(document.getText(), Module);
+                if (linePrefix.endsWith(OpenMaya + '.')) {
+                    return [...OpenMaya_1_completions[Module]];
+                }
+                // // Note 匹配获取当前函数的名称
+                // let regx = new RegExp(`${OpenMaya}\.(.*)\.`, "i");
+                // let match = linePrefix.match(regx);
+                // if (match == null) return undefined;
+                // // Note 获取数组中最后一个符合条件的对象
+                // let Class = match[match.length - 1];
+                // if (Class in OpenMaya_1_completions){
+                // 	return [...OpenMaya_1_completions[Class]];
+                // }
+            }
+            return undefined;
         }
     }, '.' // NOTE triggered whenever a '.' is being typed
     );
-    const cmds_args_compeletion = vscode.languages.registerCompletionItemProvider('python', {
+    // NOTE cmds 命令行的参数快速显示
+    const args_compeletion = vscode.languages.registerCompletionItemProvider('python', {
         provideCompletionItems(document, position) {
             let cmds_args = [];
             let cmds = getImportName(document.getText(), "cmds");
@@ -144,30 +179,43 @@ function activate(context) {
             let linePrefix = document.lineAt(position).text.substr(0, position.character);
             if (linePrefix.search(/\(/i) == -1)
                 return undefined;
-            let match = linePrefix.split("(")[0].match(new RegExp(`${cmds}\.(.*)`, "i"));
+            // Note 匹配获取当前函数的名称
+            let regx = new RegExp(`${cmds}\.(.*)\s*\(`, "i");
+            let match = linePrefix.match(regx);
             if (match == null)
                 return undefined;
-            let func = match[1];
-            cmds_data['completions'][func]['param'].forEach(this_item => {
-                let item = new vscode.CompletionItem(`${this_item['shortName']}=`, vscode.CompletionItemKind.Function);
+            // Note 获取数组中最后一个符合条件的对象
+            let func = match[match.length - 1];
+            // Note 更具匹配的函数获取参数
+            cmds_data[func]['param'].forEach(this_item => {
+                let item = new vscode.CompletionItem(`${this_item['shortName']}=`, vscode.CompletionItemKind.TypeParameter);
                 item.detail = `${this_item['longName']} [${this_item['type']}]`;
                 item.documentation = this_item['instruction'];
                 cmds_args.push(item);
-                item = new vscode.CompletionItem(`${this_item['longName']}=`, vscode.CompletionItemKind.Function);
+                item = new vscode.CompletionItem(`${this_item['longName']}=`, vscode.CompletionItemKind.TypeParameter);
                 item.detail = `${this_item['longName']} [${this_item['type']}]`;
                 item.documentation = this_item['instruction'];
                 cmds_args.push(item);
             });
-            Logger.info(`mode: ${cmds_data['completions'][func]['mode']}`);
-            for (let mode in cmds_data['completions'][func]['mode']) {
+            // NOTE 添加 query 和 edit 模式
+            for (let index in cmds_data[func]['mode']) {
+                let mode = cmds_data[func]['mode'][index];
                 if (mode == "query") {
-                    let item = new vscode.CompletionItem(`q=`, vscode.CompletionItemKind.Function);
+                    let item = new vscode.CompletionItem(`query=1`, vscode.CompletionItemKind.TypeParameter);
+                    item.detail = `query [boolean]`;
+                    item.documentation = `enable query mode`;
+                    cmds_args.push(item);
+                    item = new vscode.CompletionItem(`q=1`, vscode.CompletionItemKind.TypeParameter);
                     item.detail = `query [boolean]`;
                     item.documentation = `enable query mode`;
                     cmds_args.push(item);
                 }
                 else if (mode == "edit") {
-                    let item = new vscode.CompletionItem(`e=`, vscode.CompletionItemKind.Function);
+                    let item = new vscode.CompletionItem(`edit=1`, vscode.CompletionItemKind.TypeParameter);
+                    item.detail = `edit [boolean]`;
+                    item.documentation = `enable edit mode`;
+                    cmds_args.push(item);
+                    item = new vscode.CompletionItem(`e=1`, vscode.CompletionItemKind.TypeParameter);
                     item.detail = `edit [boolean]`;
                     item.documentation = `enable edit mode`;
                     cmds_args.push(item);
@@ -176,8 +224,8 @@ function activate(context) {
             return [...cmds_args];
         }
     }, '(', ',');
-    // Logger.info(`data: ${data['completions']}`);
-    context.subscriptions.push(cmds_func_compeletion, cmds_args_compeletion);
+    // Logger.info(`data: ${data}`);
+    context.subscriptions.push(func_compeletion, args_compeletion);
 }
 exports.activate = activate;
 //# sourceMappingURL=extension.js.map
--- src/extension.ts
+++ src/extension.ts
@@ -5,6 +5,7 @@
 import * as vscode from 'vscode';
 import * as mel_data from './data/mel.json';
 import * as cmds_data from './data/cmds.json';
+import * as OpenMaya_1_data from './data/OpenMaya1.0.json';
 
 
 export class TimeUtils {
@@ -55,54 +56,51 @@ export class Logger {
 
 export function activate(context: vscode.ExtensionContext) {
 
-
-
 	let outputPanel = vscode.window.createOutputChannel('Maya Dev');
 	Logger.registerOutputPanel(outputPanel);
 
+	// NOTE Python Extension API 测试
+	// let pythonExt = vscode.extensions.getExtension('ms-python.python');
+	// let api = pythonExt.exports;
 
-	let pythonExt = vscode.extensions.getExtension('ms-python.python');
-	let api = pythonExt.exports;
+	// Logger.info(`pythonExt: ${Object.keys(pythonExt)}`);
+	// Logger.info(`pythonExt.id: ${pythonExt.id}`);
+	// Logger.info(`pythonExt.extensionPath: ${pythonExt.extensionPath}`);
+	// Logger.info(`api: ${Object.keys(api)}`);
+	// api.ready.then(values => {
+	// 	Logger.info(`=====================================`);
+	// 	Logger.info(`==========extension ready============`);
+	// 	Logger.info(`=====================================`);
+	// 	Logger.info(`values : ${typeof values}`);
+	// 	Logger.info(`values : ${Object.keys(values)}`); 
+	// 	Logger.info(`values.length : ${Object.keys(values).length}`); 
+	// 	for (const key in values) {
+	// 		Logger.info(`key : ${key}`); 
+	// 		Logger.info(`typeof : ${typeof key}`); 
+	// 		Logger.info(`hasOwnProperty : ${values.hasOwnProperty(key)}`); 
+	// 	}
+	// 	Logger.info(`=====================================`); 
+	// 	Logger.info(`=====================================`); 
+	// 	Logger.info(`=====================================`); 
+	// 	let pythonConfig = vscode.workspace.getConfiguration("python")
+	// 	let pythonPath = pythonConfig.get("pythonPath")
+	// 	Logger.info(`pythonConfig : ${pythonConfig}`); 
+	// 	Logger.info(`pythonConfig keys : ${Object.keys(pythonConfig)}`); 
+	// 	for (const key in pythonConfig) {
+	// 		Logger.info(`key : ${key}`);
+	// 		Logger.info(`typeof : ${typeof key}`);
+	// 		Logger.info(`hasOwnProperty : ${values.hasOwnProperty(key)}`);
+	// 	}
+	// 	Logger.info(`pythonPath : ${pythonPath}`); 
+	// })
 
-	Logger.info(`pythonExt: ${Object.keys(pythonExt)}`);
-	Logger.info(`pythonExt.id: ${pythonExt.id}`);
-	Logger.info(`pythonExt.extensionPath: ${pythonExt.extensionPath}`);
-	Logger.info(`api: ${Object.keys(api)}`);
-	api.ready.then(values => {
-		Logger.info(`=====================================`);
-		Logger.info(`==========extension ready============`);
-		Logger.info(`=====================================`);
-		Logger.info(`values : ${typeof values}`);
-		Logger.info(`values : ${Object.keys(values)}`); 
-		Logger.info(`values.length : ${Object.keys(values).length}`); 
-		for (const key in values) {
-			Logger.info(`key : ${key}`); 
-			Logger.info(`typeof : ${typeof key}`); 
-			Logger.info(`hasOwnProperty : ${values.hasOwnProperty(key)}`); 
-		}
-		Logger.info(`=====================================`); 
-		Logger.info(`=====================================`); 
-		Logger.info(`=====================================`); 
-		let pythonConfig = vscode.workspace.getConfiguration("python")
-		let pythonPath = pythonConfig.get("pythonPath")
-		Logger.info(`pythonConfig : ${pythonConfig}`); 
-		Logger.info(`pythonConfig keys : ${Object.keys(pythonConfig)}`); 
-		for (const key in pythonConfig) {
-			Logger.info(`key : ${key}`);
-			Logger.info(`typeof : ${typeof key}`);
-			Logger.info(`hasOwnProperty : ${values.hasOwnProperty(key)}`);
-		}
-		Logger.info(`pythonPath : ${pythonPath}`); 
-	})
-
-
-	let mel_completions: Array<vscode.CompletionItem> = [];
-	let cmds_completions: Array<vscode.CompletionItem> = [];
 
 	// NOTE 初始化插件的时候 
 
+
 	// // NOTE 获取 MEL 数据存放到数组当中
-	// mel_data['completions'].forEach(this_item => {
+	// let mel_completions: Array<vscode.CompletionItem> = [];
+	// mel_data.forEach(this_item => {
 	// 	let item = new vscode.CompletionItem(this_item['trigger'], vscode.CompletionItemKind.Function);
 	// 	item.detail = this_item['trigger'];
 	// 	item.documentation = this_item['comment'];
@@ -110,16 +108,30 @@ export function activate(context: vscode.ExtensionContext) {
 	// });
 
 	// NOTE 获取 cmds 数据存放到数组当中
-	for (let command in cmds_data['completions']) {
+	let cmds_completions: Array<vscode.CompletionItem> = [];
+	for (let command in cmds_data) {
 		let item = new vscode.CompletionItem(command, vscode.CompletionItemKind.Function);
 		item.detail = command;
 
-		// doc = cmds['completions'][command]['instruction'];
-		const instruction = new vscode.MarkdownString(cmds_data['completions'][command]['instruction']);
+		// doc = cmds[command]['instruction'];
+		const instruction = new vscode.MarkdownString(cmds_data[command]['instruction']);
 		instruction.isTrusted = true;
 		item.documentation = instruction;
 		cmds_completions.push(item);
 	}
+
+	let OpenMaya_1_completions = {};
+	for (let Module in OpenMaya_1_data) {
+		let Class_completions = [];
+		for (let Class in OpenMaya_1_data[Module]) {
+			let item = new vscode.CompletionItem(Class, vscode.CompletionItemKind.Class);
+			item.detail = OpenMaya_1_data[Module][Class]["instruction"];
+			Class_completions.push(item)
+		}
+		OpenMaya_1_completions[Module] = Class_completions
+	}
+
+
 
 	function getImportName(documentText: string, pacakge: string) {
 		let cmds = pacakge;
@@ -142,24 +154,53 @@ export function activate(context: vscode.ExtensionContext) {
 		return cmds;
 	}
 
-	const cmds_func_compeletion = vscode.languages.registerCompletionItemProvider(
+	// NOTE cmds 函数名快速显示
+	const func_compeletion = vscode.languages.registerCompletionItemProvider(
 		'python',
 		{
 			provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
 
+				// NOTE `document.lineAt(position).text`  获取当前光标所在行的文本 并且切掉光标之后的内容
+				let linePrefix = document.lineAt(position).text.substr(0, position.character);
+
+				// ! -------------------------------
+				// !           cmds 提示
+				// ! -------------------------------
 				let cmds = getImportName(document.getText(), "cmds");
 
-				// NOTE `document.lineAt(position).text`  获取当前光标所在行的文本
-				let linePrefix = document.lineAt(position).text.substr(0, position.character);
-				if (!linePrefix.endsWith(cmds + '.')) return undefined;
+				if (linePrefix.endsWith(cmds + '.'))
+					return [...cmds_completions];
 
-				return [...cmds_completions];
+				// ! -------------------------------
+				// !        OpenMaya 1.0 提示
+				// ! -------------------------------
+
+				for (let Module in OpenMaya_1_data) {
+					let OpenMaya = getImportName(document.getText(), Module);
+
+					if (linePrefix.endsWith(OpenMaya + '.')) {
+						return [...OpenMaya_1_completions[Module]];
+					}
+
+					// // Note 匹配获取当前函数的名称
+					// let regx = new RegExp(`${OpenMaya}\.(.*)\.`, "i");
+					// let match = linePrefix.match(regx);
+					// if (match == null) return undefined;
+					// // Note 获取数组中最后一个符合条件的对象
+					// let Class = match[match.length - 1];
+					// if (Class in OpenMaya_1_completions){
+					// 	return [...OpenMaya_1_completions[Class]];
+					// }
+				}
+
+				return undefined;
 			}
 		},
 		'.' // NOTE triggered whenever a '.' is being typed
 	);
 
-	const cmds_args_compeletion = vscode.languages.registerCompletionItemProvider(
+	// NOTE cmds 命令行的参数快速显示
+	const args_compeletion = vscode.languages.registerCompletionItemProvider(
 		'python',
 		{
 			provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
@@ -171,30 +212,44 @@ export function activate(context: vscode.ExtensionContext) {
 				// NOTE `document.lineAt(position).text`  获取当前光标所在行的文本
 				let linePrefix = document.lineAt(position).text.substr(0, position.character);
 				if (linePrefix.search(/\(/i) == -1) return undefined;
-				let match = linePrefix.split("(")[0].match(new RegExp(`${cmds}\.(.*)`, "i"));
-				if (match == null) return undefined;
-				let func = match[1];
 
-				cmds_data['completions'][func]['param'].forEach(this_item => {
-					let item = new vscode.CompletionItem(`${this_item['shortName']}=`, vscode.CompletionItemKind.Function);
+				// Note 匹配获取当前函数的名称
+				let regx = new RegExp(`${cmds}\.(.*)\s*\(`, "i");
+				let match = linePrefix.match(regx);
+				if (match == null) return undefined;
+				// Note 获取数组中最后一个符合条件的对象
+				let func = match[match.length - 1];
+
+				// Note 更具匹配的函数获取参数
+				cmds_data[func]['param'].forEach(this_item => {
+					let item = new vscode.CompletionItem(`${this_item['shortName']}=`, vscode.CompletionItemKind.TypeParameter);
 					item.detail = `${this_item['longName']} [${this_item['type']}]`;
 					item.documentation = this_item['instruction'];
 					cmds_args.push(item);
-					item = new vscode.CompletionItem(`${this_item['longName']}=`, vscode.CompletionItemKind.Function);
+					item = new vscode.CompletionItem(`${this_item['longName']}=`, vscode.CompletionItemKind.TypeParameter);
 					item.detail = `${this_item['longName']} [${this_item['type']}]`;
 					item.documentation = this_item['instruction'];
 					cmds_args.push(item);
 				});
 
-				Logger.info(`mode: ${cmds_data['completions'][func]['mode']}`);
-				for (let mode in cmds_data['completions'][func]['mode']) {
+				// NOTE 添加 query 和 edit 模式
+				for (let index in cmds_data[func]['mode']) {
+					let mode = cmds_data[func]['mode'][index];
 					if (mode == "query") {
-						let item = new vscode.CompletionItem(`q=`, vscode.CompletionItemKind.Function);
+						let item = new vscode.CompletionItem(`query=1`, vscode.CompletionItemKind.TypeParameter);
+						item.detail = `query [boolean]`;
+						item.documentation = `enable query mode`;
+						cmds_args.push(item);
+						item = new vscode.CompletionItem(`q=1`, vscode.CompletionItemKind.TypeParameter);
 						item.detail = `query [boolean]`;
 						item.documentation = `enable query mode`;
 						cmds_args.push(item);
 					} else if (mode == "edit") {
-						let item = new vscode.CompletionItem(`e=`, vscode.CompletionItemKind.Function);
+						let item = new vscode.CompletionItem(`edit=1`, vscode.CompletionItemKind.TypeParameter);
+						item.detail = `edit [boolean]`;
+						item.documentation = `enable edit mode`;
+						cmds_args.push(item);
+						item = new vscode.CompletionItem(`e=1`, vscode.CompletionItemKind.TypeParameter);
 						item.detail = `edit [boolean]`;
 						item.documentation = `enable edit mode`;
 						cmds_args.push(item);
@@ -206,9 +261,9 @@ export function activate(context: vscode.ExtensionContext) {
 		'(', ','
 	);
 
-	// Logger.info(`data: ${data['completions']}`);
+	// Logger.info(`data: ${data}`);
 	context.subscriptions.push(
-		cmds_func_compeletion,
-		cmds_args_compeletion,
+		func_compeletion,
+		args_compeletion,
 	);
 }
